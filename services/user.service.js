f45c254bb5f56f9f7b9162eabcd2fe430f7d0076
Gustavo-satr <gus-jar@outlook.com>
2023-07-31 00:08:42 -0600

    Implemetando login con Passport.js

--- services/user.service.js
+++ services/user.service.js
@@ -1,10 +1,8 @@
 const boom = require('@hapi/boom');
-
+const bcrypt = require('bcrypt');
 const { models } = require('./../libs/sequelize');
-
 class UserService {
   constructor() {}
-
   async create(data) {
     const hash = await bcrypt.hash(data.password, 10);
     const newUser = await models.User.create({
@@ -14,13 +12,20 @@ class UserService {
     delete newUser.dataValues.password;
     return newUser;
   }
-
   async find() {
     const rta = await models.User.findAll({
       include: ['customer']
     });
     return rta;
   }
+
+  async findByEmail(email) {
+    const rta = await models.User.findOne({
+      where : { email }
+    });
+    return rta;
+  }
+
   async findOne(id) {
     const user = await models.User.findByPk(id);
     if (!user) {
